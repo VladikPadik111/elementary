176bf5bd85fa27c425090bc34cc7f1c8d8d5d1de
VladikPadik111 <132107262+VladikPadik111@users.noreply.github.com>
2024-03-15 15:56:35 +0300

    1.10 Идеальный вес

---
+++ src/main/java/ru/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height1 = 187;
+        short height2 = 160;
+        double man = Fit.manWeight(height1);
+        double woman = Fit.womanWeight(height2);
+        System.out.println("Man 187 is " + man);
+        System.out.println("Woman 160 is " + woman);
+    }
+
+
+}
