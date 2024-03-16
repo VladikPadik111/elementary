80ca452407936c8fcb2e11b857353481fd04f2b6
VladikPadik111 <132107262+VladikPadik111@users.noreply.github.com>
2024-03-16 14:59:14 +0300

    1.14 String

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie ";
+        int year = 2024;
+        System.out.println(idea + year);
+    }
+
+}
